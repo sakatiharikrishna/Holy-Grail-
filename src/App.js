1ae46ba322f40ee19253dbf1c99dd6708754a8a8
sakatiharikrishna <sakatiharikrishna@gmail.com>
2023-10-26 14:53:13 +0530

    one

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+
+export default function App() {
+  return (
+    <>
+      <header>Header</header>
+      <div className="columns">
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </>
+  );
+}
+
